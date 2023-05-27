a9de6d2b4c3727d657467ff801263aec1baa594f
brandon-schabel <brandonschabel1995@gmail.com>
2023-05-26 22:35:42 -0700

    add daisyui and react-aria

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,15 @@
 import { SignedIn, SignedOut } from "@clerk/remix";
 import { Link } from "@remix-run/react";
+import {usePopover} from 'react-aria'
 // Main component using <SignedIn> and <SignedOut>
 //
 // The SignedIn and SignedOut components are used to control rendering depending
 // on whether or not a visitor is signed in.
 //
 // https://docs.clerk.dev/frontend/react/signedin-and-signedout
-const Main = () => (
+const Main = () => {
+  // const test = usePopover()
+  return(
   <main className="main">
     <h1 className="title">Welcome to your new app</h1>
     <SignedIn>
@@ -15,16 +18,20 @@ const Main = () => (
     <SignedOut>
       <p className="description">Sign up for an account to get started</p>
     </SignedOut>
+    <SignedOut>
 
-    <Link to="/sign-in">
-      Sign In
-    </Link>
+      <Link to="/sign-in">
+        Sign In
+      </Link>
 
-    <Link to="/sign-up">
-      Sign Up
-    </Link>
+      <Link to="/sign-up">
+        Sign Up
+      </Link>
+
+    </SignedOut>
+      <button className="btn btn-success" >Click Me</button>
   </main>
-);
+);}
 
 export default function Index() {
   return (
